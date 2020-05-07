7048c4803c86316880ec0c27077b40551c774678
Radek Danecek <danekradek@gmail.com>
2020-05-07 18:18:01 +0200

    Adding the inverse operation functionality to the normalize function.

--- transforms/normalize.py
+++ transforms/normalize.py
@@ -7,12 +7,19 @@ class NormalizeGeometricData(object):
         self.mean = mean
         self.std = std
 
-    def __call__(self, data):
+    def _init(self, dtype, device):
         assert self.mean is not None and self.std is not None, ('Initialize mean and std to normalize with')
-        self.mean = torch.as_tensor(self.mean, dtype=data.x.dtype, device=data.x.device)
-        self.std = torch.as_tensor(self.std, dtype=data.x.dtype, device=data.x.device)
+        self.mean = torch.as_tensor(self.mean, dtype=dtype, device=device)
+        self.std = torch.as_tensor(self.std, dtype=dtype, device=device)
+
+    def __call__(self, data):
+        self._init(data.x.dtype, data.x.device)
         data.x = (data.x - self.mean)/self.std
         data.y = (data.y - self.mean)/self.std
         return data
 
-
+    def inv(self, data):
+        self._init(data.x.dtype, data.x.device)
+        data.x = (data.x*self.std) + self.mean
+        data.y = (data.y*self.std) + self.mean
+        return data
