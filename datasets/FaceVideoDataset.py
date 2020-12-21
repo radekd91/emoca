234c42b17d25e5df8c14d7225e9681bbbae8d749
Radek Danecek <danekradek@gmail.com>
2020-12-21 14:29:06 +0100

    Add testing script for DECA with videos

--- datasets/FaceVideoDataset.py
+++ datasets/FaceVideoDataset.py
@@ -18,6 +18,10 @@ from collections import OrderedDict
 from tqdm import tqdm
 import subprocess
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'DECA')))
+# from decalib.deca import DECA
+from decalib.datasets import datasets
+
 
 class FaceVideoDataModule(pl.LightningDataModule):
 
@@ -33,7 +37,14 @@ class FaceVideoDataModule(pl.LightningDataModule):
         else:
             processed_folder = os.path.join(output_dir, processed_subfolder)
         self.output_dir = processed_folder
+
         self.version = 0
+
+        self.video_list = None
+        self.video_metas = None
+        self.annotation_list = None
+        self.frame_lists = None
+
 
     @property
     def metadata_path(self):
@@ -46,42 +57,50 @@ class FaceVideoDataModule(pl.LightningDataModule):
         if outdir.is_dir():
             print("The dataset is already processed. Loading")
             self._loadMeta()
-        else:
-            self._gather_data()
-            self._saveMeta()
+            return
+        # else:
+        self._gather_data()
         self._unpack_videos()
+        self._saveMeta()
 
 
     def _unpack_videos(self):
+        self.frame_lists = []
         for vi, video_file in enumerate(tqdm(self.video_list)):
             self._unpack_video(vi)
 
-    def _unpack_video(self, video_idx):
+    def _unpack_video(self, video_idx, overwrite=False):
         video_file = Path(self.root_dir) / self.video_list[video_idx]
         suffix = Path(video_file.parts[-4]) / video_file.parts[-3] / video_file.parts[-2] / video_file.stem
         out_folder = Path(self.output_dir) / suffix
 
-        out_folder.mkdir(exist_ok=True, parents=True)
-        # ffmpeg -r 1 -i file.mp4 -r 1 "$%06d.png"
+        if not out_folder.exists() or overwrite:
+            print("Unpacking video to '%s'" % str(out_folder))
+            out_folder.mkdir(exist_ok=True, parents=True)
+            # ffmpeg -r 1 -i file.mp4 -r 1 "$%06d.png"
 
-        out_format = out_folder / "%06d.png"
-        out_format = '-r 1 -i %s -r 1 ' % str(video_file) + ' "' + str(out_format) + '"'
-        # out_format = ' -r 1 -i %s ' % str(video_file) + ' "' + "$frame.%03d.png" + '"'
-        # subprocess.call(['ffmpeg', out_format])
-        os.system("ffmpeg " + out_format)
+            out_format = out_folder / "%06d.png"
+            out_format = '-r 1 -i %s -r 1 ' % str(video_file) + ' "' + str(out_format) + '"'
+            # out_format = ' -r 1 -i %s ' % str(video_file) + ' "' + "$frame.%03d.png" + '"'
+            # subprocess.call(['ffmpeg', out_format])
+            os.system("ffmpeg " + out_format)
 
-        # import ffmpeg
-        # stream = ffmpeg.input(str(video_file))
-        # # stream = ffmpeg.output(stream.video, str(out_format))
-        # stream = ffmpeg.output(stream.video, "%06.png")
-        # stream.run()
-
-        n_frames = len(list(out_folder.glob("*.png")))
+            # import ffmpeg
+            # stream = ffmpeg.input(str(video_file))
+            # # stream = ffmpeg.output(stream.video, str(out_format))
+            # stream = ffmpeg.output(stream.video, "%06.png")
+            # stream.run()
+        frame_list = sorted(list(out_folder.glob("*.png")))
+        frame_list = [path.relative_to(self.output_dir) for path in frame_list]
+        self.frame_lists += [frame_list]
+        n_frames = len(frame_list)
         expected_frames = int(self.video_metas[video_idx]['num_frames'])
         if n_frames == expected_frames:
-            print("Successfully unpacked the video into %d frames" % expected_frames)
+            pass
+            # print("Successfully unpacked the video into %d frames" % expected_frames)
         else:
-            print("Expected %d frames but got %d" % (expected_frames, n_frames))
+            print("[WARNING] Expected %d frames but got %d vor video '%s'"
+                  % (expected_frames, n_frames, str(video_file)))
 
     def _gather_data(self, exist_ok=False):
         print("Processing dataset")
@@ -121,17 +140,28 @@ class FaceVideoDataModule(pl.LightningDataModule):
             self.video_list = pkl.load(f)
             self.video_metas = pkl.load(f)
             self.annotation_list = pkl.load(f)
+            try:
+                self.frame_lists = pkl.load(f)
+            except Exception:
+                pass
 
     def _saveMeta(self):
         with open(self.metadata_path, "wb") as f:
             pkl.dump(self.version, f)
-            pkl.dump(self.video_list,f)
+            pkl.dump(self.video_list, f)
             pkl.dump(self.video_metas, f)
             pkl.dump(self.annotation_list,f)
+            pkl.dump(self.frame_lists, f)
 
 
     def setup(self, stage: Optional[str] = None):
-        pass
+        # is dataset already processed?
+        if not Path(self.output_dir).is_dir():
+            raise RuntimeError("The folder with the processed not not found")
+
+        print("Loading the dataset")
+        self._loadMeta()
+        print("Dataset loaded")
 
     def train_dataloader(self, *args, **kwargs) -> DataLoader:
         pass
@@ -139,11 +169,169 @@ class FaceVideoDataModule(pl.LightningDataModule):
     def val_dataloader(self, *args, **kwargs) -> Union[DataLoader, List[DataLoader]]:
         pass
 
-    def test_dataloader(self, *args, **kwargs) -> Union[DataLoader, List[DataLoader]]:
-        pass
+    def test_dataloader(self, sequence_ids, *args, **kwargs) -> Union[DataLoader, List[DataLoader]]:
 
-    def transfer_batch_to_device(self, batch: Any, device: torch.device) -> Any:
-        pass
+        if sequence_ids is None:
+            sequence_ids = list(range(self.num_sequences))
+        elif isinstance(sequence_ids, int):
+            sequence_ids = [sequence_ids, ]
+
+        test_frames = []
+        for sid in sequence_ids:
+            test_frames += self.frame_lists[sid]
+        torch.multiprocessing.set_start_method('spawn')
+        test_frames = [str( Path(self.output_dir) / frame) for frame in test_frames]
+        testdata = TestData(test_frames, iscrop=True, face_detector='fan')
+        batch_size = 1
+        return DataLoader(testdata, batch_size=batch_size, num_workers=0)
+
+
+
+    @property
+    def num_sequences(self):
+        return len(self.video_list)
+
+
+    # def transfer_batch_to_device(self, batch: Any, device: torch.device) -> Any:
+    #     pass
+
+
+import cv2
+import scipy
+from skimage.io import imread, imsave
+from skimage.transform import estimate_transform, warp, resize, rescale
+from glob import glob
+import scipy.io
+
+from decalib.datasets import detectors
+
+def video2sequence(video_path):
+    videofolder = video_path.split('.')[0]
+    util.check_mkdir(videofolder)
+    video_name = video_path.split('/')[-1].split('.')[0]
+    vidcap = cv2.VideoCapture(video_path)
+    success,image = vidcap.read()
+    count = 0
+    imagepath_list = []
+    while success:
+        imagepath = '{}/{}_frame{:04d}.jpg'.format(videofolder, video_name, count)
+        cv2.imwrite(imagepath, image)     # save frame as JPEG file
+        success,image = vidcap.read()
+        count += 1
+        imagepath_list.append(imagepath)
+    print('video frames are stored in {}'.format(videofolder))
+    return imagepath_list
+
+
+class TestData(Dataset):
+    def __init__(self, testpath, iscrop=True, crop_size=224, scale=1.25, face_detector='mtcnn'):
+        '''
+            testpath: folder, imagepath_list, image path, video path
+        '''
+        if isinstance(testpath, list):
+            self.imagepath_list = testpath
+        elif os.path.isdir(testpath):
+            self.imagepath_list = glob(testpath + '/*.jpg') + glob(testpath + '/*.png') + glob(testpath + '/*.bmp')
+        elif os.path.isfile(testpath) and (testpath[-3:] in ['jpg', 'png', 'bmp']):
+            self.imagepath_list = [testpath]
+        elif os.path.isfile(testpath) and (testpath[-3:] in ['mp4', 'csv', 'vid', 'ebm']):
+            self.imagepath_list = video2sequence(testpath)
+        else:
+            print(f'please check the test path: {testpath}')
+            exit()
+        print('total {} images'.format(len(self.imagepath_list)))
+        self.imagepath_list = sorted(self.imagepath_list)
+        self.crop_size = crop_size
+        self.scale = scale
+        self.iscrop = iscrop
+        self.resolution_inp = crop_size
+        if face_detector == 'fan':
+            self.face_detector = detectors.FAN()
+        # elif face_detector == 'mtcnn':
+        #     self.face_detector = detectors.MTCNN()
+        else:
+            print(f'please check the detector: {face_detector}')
+            exit()
+
+    def __len__(self):
+        return len(self.imagepath_list)
+
+    def bbox2point(self, left, right, top, bottom, type='bbox'):
+        ''' bbox from detector and landmarks are different
+        '''
+        if type == 'kpt68':
+            old_size = (right - left + bottom - top) / 2 * 1.1
+            center = np.array([right - (right - left) / 2.0, bottom - (bottom - top) / 2.0])
+        elif type == 'bbox':
+            old_size = (right - left + bottom - top) / 2
+            center = np.array([right - (right - left) / 2.0, bottom - (bottom - top) / 2.0 + old_size * 0.12])
+        else:
+            raise NotImplementedError
+        return old_size, center
+
+    def __getitem__(self, index):
+        imagepath = self.imagepath_list[index]
+        imagename = imagepath.split('/')[-1].split('.')[0]
+
+        image = np.array(imread(imagepath))
+        if len(image.shape) == 2:
+            image = image[:, :, None].repeat(1, 1, 3)
+        if len(image.shape) == 3 and image.shape[2] > 3:
+            image = image[:, :, :3]
+
+        h, w, _ = image.shape
+        if self.iscrop:
+            # provide kpt as txt file, or mat file (for AFLW2000)
+            kpt_matpath = imagepath.replace('.jpg', '.mat').replace('.png', '.mat')
+            kpt_txtpath = imagepath.replace('.jpg', '.txt').replace('.png', '.txt')
+            if os.path.exists(kpt_matpath):
+                kpt = scipy.io.loadmat(kpt_matpath)['pt3d_68'].T
+                left = np.min(kpt[:, 0]);
+                right = np.max(kpt[:, 0]);
+                top = np.min(kpt[:, 1]);
+                bottom = np.max(kpt[:, 1])
+                old_size, center = self.bbox2point(left, right, top, bottom, type='kpt68')
+            elif os.path.exists(kpt_txtpath):
+                kpt = np.loadtxt(kpt_txtpath)
+                left = np.min(kpt[:, 0]);
+                right = np.max(kpt[:, 0]);
+                top = np.min(kpt[:, 1]);
+                bottom = np.max(kpt[:, 1])
+                old_size, center = self.bbox2point(left, right, top, bottom, type='kpt68')
+            else:
+                bbox, bbox_type = self.face_detector.run(image)
+                if len(bbox) < 4:
+                    print('no face detected! run original image')
+                    left = 0;
+                    right = h - 1;
+                    top = 0;
+                    bottom = w - 1
+                else:
+                    left = bbox[0];
+                    right = bbox[2]
+                    top = bbox[1];
+                    bottom = bbox[3]
+                old_size, center = self.bbox2point(left, right, top, bottom, type=bbox_type)
+            size = int(old_size * self.scale)
+            src_pts = np.array(
+                [[center[0] - size / 2, center[1] - size / 2], [center[0] - size / 2, center[1] + size / 2],
+                 [center[0] + size / 2, center[1] - size / 2]])
+        else:
+            src_pts = np.array([[0, 0], [0, h - 1], [w - 1, 0]])
+
+        DST_PTS = np.array([[0, 0], [0, self.resolution_inp - 1], [self.resolution_inp - 1, 0]])
+        tform = estimate_transform('similarity', src_pts, DST_PTS)
+
+        image = image / 255.
+
+        dst_image = warp(image, tform.inverse, output_shape=(self.resolution_inp, self.resolution_inp))
+        dst_image = dst_image.transpose(2, 0, 1)
+        return {'image': torch.tensor(dst_image).float(),
+                'image_name': imagename,
+                'image_path': imagepath,
+                # 'tform': tform,
+                # 'original_image': torch.tensor(image.transpose(2,0,1)).float(),
+                }
 
 
 def main():
