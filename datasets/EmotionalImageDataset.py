Fix bug - the recent changes disabled landmark normalization which is crucial

datasets/EmotionalImageDataset.py
@@ -222,6 +222,7 @@ class EmotionalImageDataset(torch.utils.data.Dataset):
         if segmentation_list is not None and len(segmentation_list) != len(segmentation_list):
             raise RuntimeError("There must be a segmentation for every image")
         self.segmentation_list = segmentation_list
+        self.landmark_normalizer = KeypointNormalization()
         # self.segmentation_transform = segmentation_transform
         self.segmentation_discarded_labels = segmentation_discarded_lables
         self.K = K  # how many samples of one identity to get?
@@ -308,6 +309,8 @@ class EmotionalImageDataset(torch.utils.data.Dataset):
 
         if landmark is not None:
             landmark = np.squeeze(landmark)
+            self.landmark_normalizer.set_scale(img.shape[1], img.shape[2])
+            landmark = self.landmark_normalizer(landmark)
 
         sample = {
             "image": numpy_image_to_torch(img),
@@ -396,7 +399,8 @@ class EmotionalImageDataset(torch.utils.data.Dataset):
             index_axis(k, i).imshow(sample["image"][k, ...].numpy().transpose([1,2,0]))
             i += 1
             if 'landmark' in sample.keys():
-                lmk_im = tensor_vis_landmarks(sample['image'][k:k+1, ...], sample['landmark'][k:k+1, ...],
+                lmk_im = tensor_vis_landmarks(sample['image'][k:k+1, ...],
+                                              self.landmark_normalizer.inv(sample['landmark'][k:k+1, ...]),
                                               isScale=False, rgb2bgr=False, scale_colors=True).numpy()[0]\
                     .transpose([1,2,0])
                 index_axis(k, i).imshow(lmk_im)

transforms/keypoints.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 class KeypointTransform(torch.nn.Module):
@@ -26,19 +27,38 @@ class KeypointScale(KeypointTransform):
         points_[..., 1] *= self.scale_y
         return points_
 
+
 class KeypointNormalization(KeypointTransform):
 
     def __init__(self, scale_x=1., scale_y=1.):
         super().__init__(scale_x, scale_y)
 
     def forward(self, points):
-        points_ = points.clone()
         # normalization the way DECA uses it.
         # the keypoints are not used in image space but in normalized space
         # for loss computation
         # the normalization is as follows:
+        if isinstance(points, torch.Tensor):
+            points_ = points.clone()
+        elif isinstance(points, np.ndarray):
+            points_ = points.copy()
+        else:
+            raise ValueError(f"Invalid type of points {str(type(points))}")
         points_[..., 0] -= self.scale_x/2
         points_[..., 0] /= self.scale_x/2
         points_[..., 1] -= self.scale_y/2
         points_[..., 1] /= self.scale_y/2
+        return points_
+
+    def inv(self, points):
+        if isinstance(points, torch.Tensor):
+            points_ = points.clone()
+        elif isinstance(points, np.ndarray):
+            points_ = points.copy()
+        else:
+            raise ValueError(f"Invalid type of points {str(type(points))}")
+        points_[..., 0] *= self.scale_x / 2
+        points_[..., 0] += self.scale_x / 2
+        points_[..., 1] *= self.scale_y / 2
+        points_[..., 1] += self.scale_y / 2
         return points_