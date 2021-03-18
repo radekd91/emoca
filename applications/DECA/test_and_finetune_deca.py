65f22ce0a777c951fac1cf53ba51add68efeedf6
Radek Danecek <danekradek@gmail.com>
2021-03-18 18:19:21 +0100

    Fix crash in checkpoint loading.

--- applications/DECA/test_and_finetune_deca.py
+++ applications/DECA/test_and_finetune_deca.py
@@ -65,6 +65,8 @@ def locate_checkpoint(cfg, mode='latest'):
         min_value = 999999999999999.
         min_idx = -1
         for idx, ckpt in enumerate(checkpoints):
+            if ckpt.stem == 'last':
+                continue
             end_idx = str(ckpt.stem).rfind('=') + 1
             loss_value = float(str(ckpt.stem)[end_idx:])
             if loss_value <= min_value:
