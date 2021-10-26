Fix jaw gradient flow for emotion optimization.

gdl_apps/DECA/optimize_latent_space.py
@@ -148,7 +148,7 @@ class TargetJawCriterion(torch.nn.Module):
 
     @property
     def name(self):
-        return f"JawReg_{self.reference_type}_{self.loss_type}_"
+        return f"JawReg_{self.reference_type}_{self.loss_type}"
 
     def compute(self, posecode):
         jaw_pose = posecode[:, 3:]
@@ -159,6 +159,8 @@ class TargetJawCriterion(torch.nn.Module):
             jaw_pose = trans.axis_angle_to_quaternion(jaw_pose)
         elif self.reference_type == "euler":
             jaw_pose = trans.matrix_to_euler_angles(trans.axis_angle_to_matrix(jaw_pose), "XYZ")
+        else:
+            raise ValueError(f"Invalid rotaion reference type: '{self.reference_type}'")
 
         if self.loss_type == "l1":
             reg = torch.abs(jaw_pose - self.reference_pose).sum()
@@ -527,9 +529,22 @@ def optimize(deca,
         values['expcode'] = torch.autograd.Variable(values['expcode'].detach().clone(), requires_grad=True)
         parameters += [values['expcode']]
 
-    if optimize_neck_pose or optimize_jaw_pose:
-        values['posecode'] = torch.autograd.Variable(values['posecode'].detach().clone(), requires_grad=True)
-        parameters += [ values['posecode']]
+    if optimize_neck_pose:
+        neck_pose = torch.autograd.Variable(values['posecode'][:, :3].detach().clone(), requires_grad=True)
+        parameters += [neck_pose]
+    else:
+        neck_pose = values['posecode'][:, :3].detach().clone()
+
+    if optimize_jaw_pose:
+        jaw_pose = torch.autograd.Variable(values['posecode'][:, 3:].detach().clone(), requires_grad=True)
+        parameters += [jaw_pose]
+    else:
+        jaw_pose = values['posecode'][:, 3:].detach().clone()
+
+    # if optimize_neck_pose or optimize_jaw_pose:
+        # values['posecode'] = torch.autograd.Variable(values['posecode'].detach().clone(), requires_grad=True)
+        # parameters += [ values['posecode']]
+    values['posecode'] = torch.cat([neck_pose, jaw_pose], dim=1)
 
     if optimize_texture:
         values['texcode'] = torch.autograd.Variable(values['texcode'].detach().clone(), requires_grad=True)
@@ -622,6 +637,9 @@ def optimize(deca,
 
         optimizer.zero_grad()
 
+        if optimize_neck_pose or optimize_jaw_pose:
+            values['posecode'] = torch.cat([neck_pose, jaw_pose], dim=1)
+
         values_ = deca.decode(values, training=False)
         # losses_and_metrics = deca.compute_loss(values, training=False)
         losses_and_metrics = deca.compute_loss(values_, {}, training=True)
@@ -634,14 +652,6 @@ def optimize(deca,
         optimizer.step(closure=closure)
         # make sure pose vector is updated
 
-
-        if optimize_neck_pose and not optimize_jaw_pose:
-            values['posecode'] = torch.cat([values['posecode'].detach().clone()[:, :3],
-                                            value_history[0]['posecode'].detach().clone()[:, 3:].to(device=deca.device)], dim=1)
-        elif optimize_jaw_pose and not optimize_neck_pose:
-            values['posecode'] = torch.cat([value_history[0]['posecode'].detach().clone()[:, :3].to(device=deca.device),
-                                            values['posecode'].detach().clone()[:, 3:]],
-                dim=1)
 
         if visualize_progress or save_path is not None:
             save_visualization(deca, values,

gdl_apps/DECA/submit_emotion_optimization_to_cluster_v2.py
@@ -203,7 +203,7 @@ def main():
     target_images = [
         # target_image_path / "VA_Set/detections/Train_Set/119-30-848x480/000640_000.png", # Octavia
         ## target_image_path / "VA_Set/detections/Train_Set/82-25-854x480/000480_000.png", # Rachel 1
-        # target_image_path / "VA_Set/detections/Train_Set/82-25-854x480/002805_000.png", # Rachel 1
+        target_image_path / "VA_Set/detections/Train_Set/82-25-854x480/002805_000.png", # Rachel 1
         # target_image_path / "VA_Set/detections/Train_Set/82-25-854x480/003899_000.png", # Rachel 2
         # target_image_path / "VA_Set/detections/Train_Set/111-25-1920x1080/000685_000.png", # disgusted guy
         # target_image_path / "VA_Set/detections/Train_Set/122-60-1920x1080-1/001739_000.png", # crazy youtuber
@@ -247,14 +247,14 @@ def main():
         "optimize_texture": False,
         "optimize_cam": False,
         "optimize_light": False,
-        "lr": 1.0,
+        # "lr": 1.0,
         # "lr": 0.1,
         # "lr": 0.01,
-        # "lr": 0.001,
+        "lr": 0.001,
         # "optimizer_type" : "LBFGS",
         "optimizer_type" : "SGD",
-        # "max_iters": 1000,
-        "max_iters": 100,
+        "max_iters": 1000,
+        # "max_iters": 100,
         "patience": 20,
         "visualize_progress" : False,
         "visualize_result" : False,
@@ -365,9 +365,10 @@ def main():
     kw["emonet"] = emonet
     kw["optimize_detail"] = False
     # kw["optimize_detail"] = False
+    # kw["optimize_expression"] = True
     kw["optimize_expression"] = True
     kw["optimize_neck_pose"] = False
-    # kw["optimize_jaw_pose"] = True
+    # kw["optimize_jaw_pose"] = False
     kw["optimize_jaw_pose"] = True
     kw["losses_to_use"] = {
         # "emotion_f1": 1.,
@@ -381,14 +382,27 @@ def main():
         "loss_expression_reg": 10.,
         # "loss_z_reg" : 10.,
         "jaw_reg": {
-            "loss_type": "l1",
-            # "loss_type": "l2",
+            # "loss_type": "l1",
+            "loss_type": "l2",
             "reference_type": "euler",
-            "reference_pose": torch.deg2rad(torch.tensor([15., 0., 0.])).numpy().tolist(),
-            # "reference_type": "quat",
-            # "reference_pose": trans.euler_angles_to_quaternion(torch.deg2rad([0., 0., 0.])).numpy().tolist(),
-            "weight" : 0.1,
-        }
+            # "reference_pose": torch.deg2rad(torch.tensor([0., 0., 0.])).numpy().tolist(),
+            "reference_pose": torch.deg2rad(torch.tensor([5., 0., 0.])).numpy().tolist(),
+            # "weight" : 0.1,
+            # "weight" : 1.0,
+            "weight" : 10.0,
+            # "weight" : 100., # mouth opens, loss does minimize, but the mouth stays open a little too much
+            # "weight" : 50.,
+        },
+        # "jaw_reg": {
+        #     # "loss_type": "l1",
+        #     "loss_type": "l2",
+        #     "reference_type": "quat",
+        #     "reference_pose": trans.matrix_to_quaternion(trans.euler_angles_to_matrix(
+        #         torch.deg2rad(torch.tensor([0., 0., 0.])), "XYZ")).numpy().tolist(),
+        #     # "weight" : 0.1,
+        #     "weight" : 100.,
+        #     # "weight": 50.,
+        # }
     }
 
 