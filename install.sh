1b654a729563176dc39d9e2b0e2df5aaea059641
rdanecek <danekradek@gmail.com>
2022-04-07 10:57:36 +0200

    Relax Cython version

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mamba env create python=3.6 --file conda-environment_py36_cu11_ubuntu.yml
 eval "$(conda shell.bash hook)" # make sure conda works in the shell script
 conda activate work36_cu11
 echo "Installing GDL"
-pip install Cython==0.29.14
+pip install Cython==0.29
 pip install -e . 
 echo "Making sure Pytorch3D installed correctly"
 pip install git+https://github.com/facebookresearch/pytorch3d.git@v0.6.0
