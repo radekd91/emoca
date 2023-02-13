a18e37b74c724ce46d8485e58de4e9769c189b77
rdanecek <danekradek@gmail.com>
2023-02-13 15:53:16 +0100

    Further finetune the install script

--- install_38.sh
+++ install_38.sh
@@ -5,7 +5,7 @@ echo "Installing mamba"
 conda install mamba -n base -c conda-forge
 if ! command -v mamba &> /dev/null
 then
-    echo "mamba could not be found. The installation must have failed. Please install mamba before running this script."
+    echo "mamba could not be found. Please install mamba before running this script"
     exit
 fi
 echo "Creating conda environment"
@@ -21,14 +21,11 @@ else
 fi
 echo "Installing conda packages"
 mamba env update -n work38 --file conda-environment_py38_cu11_ubuntu.yml 
-echo "Installing GDL"
+echo "Installing other requirements"
+pip install -r requirements38.txt
 pip install Cython==0.29
-pip install -e . 
 echo "Making sure Pytorch3D installed correctly"
 pip install git+https://github.com/facebookresearch/pytorch3d.git@v0.6.2
-echo "Installing other requirements"
+echo "Installing GDL"
 pip install -e . 
-cd .. 
-pip install -r requirements38.txt
-cd ../..
 echo "Installation finished"
